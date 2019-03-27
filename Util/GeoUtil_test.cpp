2a5dd6f2ac2e7d87110e39d3d6f66ce5165cb549
wenyan4work@gmail.com <wenyan4work@gmail.com>
2019-03-27 10:17:03 -0400

    add cmake tests

--- Util/GeoUtil_test.cpp
+++ Util/GeoUtil_test.cpp
@@ -1,39 +1,68 @@
 #include "GeoUtil.hpp"
+#include "Util/TRngPool.hpp"
 
-#include <cstdio>
 #include <cmath>
+#include <cstdio>
 
-void testFindImage() {
-    const double lb = 0, ub = 1;
-    double x;
-    x = -0.1;
-    findPBCImage(lb, ub, x);
-    printf("%g\n", x);
-    x = 0.1;
-    findPBCImage(lb, ub, x);
-    printf("%g\n", x);
-    x = 1.1;
-    findPBCImage(lb, ub, x);
-    printf("%g\n", x);
+bool testFindPBCImage(double lb, double ub, double x) {
+    double ximg = x;
+    findPBCImage(lb, ub, ximg);
+
+    bool pass = (ximg < ub) && (ximg > lb);
+    double jump = (ximg - x) / (ub - lb); // should be an integer
+    pass = pass && (jump - round(jump) < 1e-8);
+
+    if (!pass) {
+        printf("%g,%g,%g,%g\n", lb, ub, x, ximg);
+    }
+
+    return pass;
 }
 
-void testFindImage(double trg) {
-    const double lb = 0, ub = 1;
-    double x;
-    x = -0.1;
-    findPBCImage(lb, ub, x, trg);
-    printf("%g\n", x);
-    x = 0.1;
-    findPBCImage(lb, ub, x, trg);
-    printf("%g\n", x);
-    x = 1.1;
-    findPBCImage(lb, ub, x, trg);
-    printf("%g\n", x);
+bool testFindPBCImageTrg(double lb, double ub, double x, double trg) {
+
+    double trgimg = trg;
+    findPBCImage(lb, ub, trgimg);
+
+    findPBCImage(lb, ub, x, trg); // x can be out of bound [lb,ub)
+
+    // x-trg has minimum distance between all images of x
+    bool pass = (fabs(trg - trgimg) < (ub - lb) / 1e10)          //
+                && fabs(x - trg) < fabs(x + (ub - lb) - trg)     //
+                && fabs(x - trg) < fabs(x - (ub - lb) - trg)     //
+                && fabs(x - trg) < fabs(x + 2 * (ub - lb) - trg) //
+                && fabs(x - trg) < fabs(x - 2 * (ub - lb) - trg);
+
+    if (!pass) {
+        printf("%g,%g,%g,%g,%g\n", lb, ub, x, trg, trgimg);
+    }
+
+    return pass;
 }
 
 int main() {
-    testFindImage();
-    testFindImage(0.3);
-    testFindImage(1.9);
+    bool pass = true;
+
+    TRngPool rngPool(10);
+
+    for (int i = 0; i < 100; i++) {
+        double lb = -rngPool.getU01() * 10 - 1;
+        double ub = rngPool.getU01() * 10 + 1;
+        double x = rngPool.getU01() * 1000 - 500;
+        pass = (pass && testFindPBCImage(lb, ub, x));
+    }
+
+    for (int i = 0; i < 100; i++) {
+        double lb = -rngPool.getU01() * 10 - 1;
+        double ub = rngPool.getU01() * 10 + 1;
+        double x = rngPool.getU01() * 1000 - 500;
+        double trg = rngPool.getU01() * 1000 - 500;
+        pass = (pass && testFindPBCImageTrg(lb, ub, x, trg));
+    }
+
+    if (pass) {
+        printf("TestPassed\n");
+    }
+
     return 0;
 }
--- Util/SortUtil_test.cpp
+++ Util/SortUtil_test.cpp
@@ -1,9 +1,9 @@
 #include "SortUtil.hpp"
 
 #include <cstdio>
+#include <random>
 
-int main() {
-    const int length = 100;
+bool test(const int length) {
     std::vector<int> tags(length);
     std::vector<int> data(length);
     // prepare initial data
@@ -11,7 +11,12 @@ int main() {
         tags[i] = i; // unique
         data[i] = i * 10;
     }
-    std::random_shuffle(tags.begin(), tags.end());
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    std::shuffle(tags.begin(), tags.end(), g);
+
     std::vector<std::pair<int, int>> combined(length);
     for (int i = 0; i < length; i++) {
         combined[i].first = tags[i];
@@ -23,12 +28,23 @@ int main() {
     std::sort(combined.begin(), combined.end());
 
     // compare
+    bool pass = true;
     for (int i = 0; i < length; i++) {
         if (data[i] != combined[i].second) {
             printf("sort mismatch at %d, data: %d, combined: %d\n", i, data[i], combined[i].second);
-        } else {
-            printf("sort result data: %d, combined: %d\n", data[i], combined[i].second);
+            pass = false;
         }
+    }
+
+    return pass;
+}
+
+int main() {
+
+    bool pass = test(1000);
+
+    if (pass) {
+        printf("TestPassed\n");
     }
 
     return 0;
