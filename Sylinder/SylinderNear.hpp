f451c2c5a05fcd464cb898b2c9a54b7db78c8539
wenyan4work <wenyan4work@gmail.com>
2020-02-04 16:50:47 -0500

    use smaller search radius

--- Sylinder/SylinderNear.hpp
+++ Sylinder/SylinderNear.hpp
@@ -105,7 +105,7 @@ struct SylinderNearEP {
      * Here length*2 ensures contact is detected with Symmetry search mode
      * @return PS::F64
      */
-    PS::F64 getRSearch() const { return (length + 2 * radiusCollision) * (1 + GEO_DEFAULT_COLBUF); }
+    PS::F64 getRSearch() const { return (length + 2 * radiusCollision) * (1 + 0.5 * GEO_DEFAULT_COLBUF); }
 
     /**
      * @brief Set pos with a PS::F64vec3 object
