8376a01aebd16bbd3216c0aa712b18dcccc64c93
wenyan4work@gmail.com <wenyan4work@gmail.com>
2020-08-21 10:38:22 -0400

    add integrator and unit test

--- Util/QuadInt.hpp
+++ Util/QuadInt.hpp
@@ -15,54 +15,152 @@
 
 #include <cmath>
 #include <cstdio>
+#include <cstdlib>
 
 /**
  * @brief class to compute Chebyshev-Gauss Quadrature
  * https://mathworld.wolfram.com/Chebyshev-GaussQuadrature.html
- * @tparam N
+ * @tparam N: max number of points
  */
 template <int N>
 class QuadInt {
+    int npts; // actual number of points
+    char choice;
     double points[N];
     double weights[N];
 
+    void calcChebQuad() {
+        /* Python code:
+         * Dkn=np.zeros((pCheb+1,pCheb+1))
+         for k in range(pCheb+1):
+         for n in range(pCheb+1):
+         Dkn[k,n]=np.cos(k*n/pCheb*np.pi)*2.0/pCheb
+         if(n==0 or n==pCheb):
+         Dkn[k,n]=np.cos(k*n/pCheb*np.pi)*1.0/pCheb
+         dvec=np.zeros(pCheb+1)
+         for i in range(pCheb+1):
+         if(i%2==1):
+         dvec[i]=0
+         else:
+         dvec[i]=2/(1.0-i**2)
+         dvec[0]=1
+         weightCC=np.dot(Dkn.transpose(),dvec)
+         *
+         * */
+        const int chebN = npts - 1;
+        double *Dkn = new double[(chebN + 1) * (chebN + 1)];
+        for (int k = 0; k < chebN + 1; k++) {
+            int n = 0;
+            Dkn[k * (chebN + 1) + n] = cos(k * n * M_PI / chebN) / chebN;
+            for (n = 1; n < chebN; n++) {
+                Dkn[k * (chebN + 1) + n] = cos(k * n * M_PI / chebN) * 2 / chebN;
+            }
+            n = chebN;
+            Dkn[k * (chebN + 1) + n] = cos(k * n * M_PI / chebN) / chebN;
+        }
+        double *dvec = new double[chebN + 1];
+        for (int i = 0; i < chebN + 1; i++) {
+            dvec[i] = i % 2 == 1 ? 0 : 2 / (1.0 - static_cast<double>(i * i));
+        }
+        dvec[0] = 1;
+        for (int i = 0; i < chebN + 1; i++) {
+            double temp = 0;
+            for (int j = 0; j < chebN + 1; j++) {
+                temp += Dkn[j * (chebN + 1) + i] * dvec[j]; // not optimal layout for speed.
+            }
+            weights[i] = temp;
+            points[i] = -cos(i * M_PI / chebN);
+        }
+
+        delete[] Dkn;
+        delete[] dvec;
+    };
+
   public:
-    QuadInt() {
+    QuadInt() : QuadInt(2) {} // default to minimal order
+
+    QuadInt(int n, char quad = 'g') : npts(n), choice(quad) {
+        if (n > N) {
+            printf("npts larger than max N.\n");
+            std::exit(1);
+        }
         // initialize points and weights
-#ifdef CHEBYSHEV
-        const double lowerB = -1;
-        const double upperB = 1;
-        for (int iQuadPt = 0; iQuadPt < N; iQuadPt++) {
-            double k = iQuadPt + 1;
-            double xi = -std::cos((2 * k - 1) * M_PI / (2 * N));
-            weights[iQuadPt] = (upperB - lowerB) / 2 * M_PI / N * std::sqrt(1 - xi * xi);
-            points[iQuadPt] = lowerB + (upperB - lowerB) * (xi + 1) / 2;
+        if (quad != 'g')
+            calcChebQuad();
+        else {
+            std::vector<double> s, w;
+            Gauss_Legendre_Nodes_and_Weights<double>(npts, s, w);
+            for (int i = 0; i < npts; i++) {
+                weights[i] = w[i];
+                points[i] = s[i];
+            }
         }
-#else
-        std::vector<double> s, w;
-        Gauss_Legendre_Nodes_and_Weights<double>(N, s, w);
-        for (int i = 0; i < N; i++) {
-            weights[i] = w[i];
-            points[i] = s[i];
-        }
-#endif
     }
 
-    int getSize() const { return N; }
+    // copy constructor
+    QuadInt(const QuadInt &other) {
+        npts = other.npts;
+        choice = other.choice;
+        for (int i = 0; i < npts; i++) {
+            points[i] = other.points[i];
+            weights[i] = other.weights[i];
+        }
+    }
+    QuadInt &operator=(const QuadInt &other) {
+        npts = other.npts;
+        choice = other.choice;
+        for (int i = 0; i < npts; i++) {
+            points[i] = other.points[i];
+            weights[i] = other.weights[i];
+        }
+    }
+
+    int getSize() const { return npts; }
+    int getMaxSize() const { return N; }
     double *getPoints() const { return points; }
     double *getWeights() const { return weights; }
 
     void print() const {
-        for (int i = 0; i < N; i++) {
+        if (choice == 'g') {
+            printf("----Gauss-Legendre Quadrature n=%d ----\n", npts);
+        } else {
+            printf("----Clenshaw-Curtis Quadrature n=%d----\n", npts);
+        }
+        for (int i = 0; i < npts; i++) {
             printf("s %12g, w %12g\n", points[i], weights[i]);
         }
     }
 
     template <class Func>
     double integrate(Func f) {
+        double fval[N];
+        for (int i = 0; i < npts; i++) {
+            fval[i] = f(points[i]);
+        }
+        return intSamples(fval);
+    }
+
+    template <class Func>
+    double integrateS(Func f) {
+        double fval[N];
+        for (int i = 0; i < npts; i++) {
+            fval[i] = f(points[i]);
+        }
+        return intSSamples(fval);
+    }
+
+    double intSamples(const double *f) {
         double sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += f(points[i]) * weights[i];
+        for (int i = 0; i < npts; i++) {
+            sum += f[i] * weights[i];
+        }
+        return sum;
+    }
+
+    double intSSamples(const double *f) {
+        double sum = 0;
+        for (int i = 0; i < npts; i++) {
+            sum += f[i] * points[i] * weights[i];
         }
         return sum;
     }
--- Util/QuadInt_test.cpp
+++ Util/QuadInt_test.cpp
@@ -1,30 +1,37 @@
 #include "QuadInt.hpp"
 
-int main() {
-    struct Integrand {
-        int e = 2;
-        double operator()(double s) const { return pow(s, e)*cos(s); }
-    };
+struct Integrand {
+    int e = 2;
+    double operator()(double s) const { return sin(exp(-2 * s)); }
+};
+
+bool test(char choice) {
     Integrand f;
-    {
-        QuadInt<2> quad;
-        quad.print();
-        printf("integral %12g\n", quad.integrate(f));
+    using Quad = QuadInt<48>;
+    std::vector<Quad> quads;
+    for (int i = 0; i < 20; i++) {
+        quads.push_back(Quad(i * 2 + 2, choice));
     }
-    {
-        QuadInt<4> quad;
-        quad.print();
-        printf("integral %12g\n", quad.integrate(f));
+    for (auto &q : quads) {
+        q.print();
     }
-    {
-        QuadInt<6> quad;
-        quad.print();
-        printf("integral %12g\n", quad.integrate(f));
+
+    for (auto &q : quads) {
+        printf("npts: %d, int[-1,1]: %18.10g, ints[-1,1]: %18.10g\n", q.getSize(), q.integrate(f), q.integrateS(f));
     }
-    {
-        QuadInt<8> quad;
-        quad.print();
-        printf("integral %12g\n", quad.integrate(f));
-    }
+
+    auto &q = quads.back();
+    double a = q.integrate(f);
+    double b = q.integrateS(f);
+
+    if (fabs(a - 0.680910296840562) < 1e-8 && fabs(b - 0.159315026657831) < 1e-8)
+        return true;
+    else
+        return false;
+}
+
+int main(int argc, char **argv) {
+    if (test('c') && test('g'))
+        printf("TestPassed\n");
     return 0;
 }
