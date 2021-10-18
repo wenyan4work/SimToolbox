87ff62c272658d1812ddb62a8c3b8eaa14b7d69e
wenyan4work <wenyan4work@gmail.com>
2021-10-18 12:21:14 -0400

    clear up TCMAT_test

---
+++ Trilinos/TCMAT_test.cpp
@@ -0,0 +1,60 @@
+#include "TpetraUtil.hpp"
+#include "Util/Logger.hpp"
+
+#include <vector>
+
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+
+  MPI_Init(&argc, &argv);
+  int rank, nProcs;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
+  {
+    Logger::setup_mpi_spdlog();
+    auto commRcp = getMPIWORLDTCOMM();
+
+    using LO = TMAP::local_ordinal_type;
+    using GO = TMAP::global_ordinal_type;
+
+    // create a sparse matrix with colMap and rowMap
+    // CrsMat : nProcs x 2 * nProcs
+    // row map: 1 row per rank
+    // col map: 2 per rank
+    std::vector<GO> gRowIndexOnLocal = {commRcp->getRank()};
+    std::vector<GO> gColIndexOnLocal = {1 + commRcp->getRank(),
+                                        3 + commRcp->getRank()};
+
+    auto rowMapRcp = getTMAPFromLocalSize(gRowIndexOnLocal.size(), commRcp);
+    auto colMapRcp = getTMAPFromGlobalIndexOnLocal(gColIndexOnLocal, //
+                                                   2 * nProcs, commRcp);
+
+    // entries
+    // const Kokkos::View< row_offset_type * > & 	rowPointers,
+    // const Kokkos::View< LocalOrdinal * > & 	columnIndices,
+    using row_offset_type =
+        TCMAT::local_matrix_type::row_map_type::non_const_value_type;
+    Kokkos::View<row_offset_type *> rowPointers("rowPointers",
+                                                gRowIndexOnLocal.size() + 1);
+    rowPointers[0] = 0;
+    rowPointers[1] = rowPointers[0] + gColIndexOnLocal.size();
+    Kokkos::View<LO *> columnIndices("columnIndices", rowPointers[1]);
+    Kokkos::View<double *> values("values", rowPointers[1]);
+    for (long i = 0; i < gColIndexOnLocal.size(); i++) {
+      columnIndices[i] = i; // local index
+      values[i] = rank;
+    }
+
+    Teuchos::RCP<TCMAT> matTestRcp = Teuchos::rcp(
+        new TCMAT(rowMapRcp, colMapRcp, rowPointers, columnIndices, values));
+
+    auto domainMapRcp = getTMAPFromLocalSize(2, commRcp);
+    auto rangeMapRcp = getTMAPFromLocalSize(1, commRcp);
+    matTestRcp->fillComplete(domainMapRcp, rangeMapRcp); // domainMap, rangeMap
+
+    dumpTCMAT(matTestRcp, "matTestRcp");
+  }
+  MPI_Finalize();
+  return 0;
+}
