9bcd37fd7d034ca219d3a7a1e8b86e81ce06a548
wenyan4work <wenyan4work@gmail.com>
2021-11-17 13:45:07 -0500

    fetch arborx

--- do-cmake.sh
+++ do-cmake.sh
@@ -7,10 +7,5 @@ cmake \
   -D CMAKE_C_COMPILER=mpicc \
   -D CMAKE_BUILD_TYPE=Release \
   -D Eigen3_DIR="${USER_LOCAL}/share/eigen3/cmake" \
-  -D TRNG_INCLUDE_DIR="${USER_LOCAL}/include" \
-  -D TRNG_LIBRARY="${USER_LOCAL}/lib/libtrng4.a" \
   -D Trilinos_DIR="${USER_LOCAL}/lib/cmake/Trilinos" \
-  -D VTK_DIR="${USER_LOCAL}/lib/cmake/vtk-9.0" \
-  -D yaml-cpp_DIR="${USER_LOCAL}/share/cmake/yaml-cpp" \
-../
-
+  ../
